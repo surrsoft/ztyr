a6e8a4abd20f7a9b733937f863f912ba1a96982a
surr <e5535@mail.ru>
2020-03-21 19:24:40 +0300

    c10

--- zint_gen.js
+++ zint_gen.js
@@ -1,29 +1,6 @@
-
-var ztyrProjectName = 'ztyr-name-generator';
-var ztyrDropboxAccessToken = '';
-
-function generatorHandler(input){
-    const engName = generateEng();
-    input.val(engName);
-}
-
-function saveHandler(){
-    console.log('===> saveHandler()');
-    dropboxFileGet('tmp.txt');
-}
-
-
-// --- dropbox
-
-function dropboxFileGet(fileName){
-    console.log('Dropbox', Dropbox);
-    const dropboxOj = new Dropbox.Dropbox({ accessToken: ztyrDropboxAccessToken, fetch: fetch });
-    console.log('dropboxOj', dropboxOj);    
-
-}
-
-
-// ---
+/*
+Модуль для генерации [zint]-терминов
+ */
 
 //генерирует английский [zint]-термин
 function generateEng() {
@@ -37,9 +14,23 @@ function generateEng() {
     return stAlphabet1.charAt(r_1) + stAlphabet1.charAt(r_2) + stAlphabet1.charAt(r_3) + stAlphabet2.charAt(r_4);
 }
 
+//генерирует кирилический [zint]-термин
+function generateRu() {
+    const st1 = "абвгдезиклмнопрстуфхюя";
+    const st2 = "абвгдезиклмнопрстуфхюя";
+    //---
+    const r_1 = randomInt(0, st1.length - 1);
+    const r_2 = randomInt(0, st1.length - 1);
+    const r_3 = randomInt(0, st1.length - 1);
+    const r_4 = randomInt(0, st2.length - 1);
+    const genSt = st1.charAt(r_1) + st1.charAt(r_2) + st1.charAt(r_3) + st2.charAt(r_4);
+}
+
 //генерирует рандомное целое число в диапазоне [min] [max]
 function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// ---
+//---
+exports.generateEng = generateEng;
+exports.generateRu = generateRu;
