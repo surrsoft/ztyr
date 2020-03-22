87cb38d81c75598f0f42f3a9a2a5a70d10955553
surr <e5535@mail.ru>
2020-03-22 07:42:44 +0300

    cc

--- main.js
+++ main.js
@@ -11,23 +11,9 @@ function saveHandler() {
   console.log('===> saveHandler()');
 
   // ---
-  dropbox('files/download', { path: ZTYR_FILE_NAME }, {
-    onComplete: function (result, resp) {
-      console.log('!!-!!-!! -> onComplete() {200322061108}'); // del+
-      console.log('!!-!!-!! result {200322060448}\n', result); // del+
-      console.log('!!-!!-!! resp {200322063834}\n', resp); // del+
-      resp.text()
-        .then(function (text) {
-          console.log('!!-!!-!! text {200322064020}\n', text); // del
-        })
-        .catch(function (err) {
-          console.log('!!-!!-!! err {200322064035}\n', err); // del
-        })
-    },
-  });
+  paemDropboxFileDownload(ZTYR_FILE_NAME);
 
 }
-
 
 // --- dropbox
 
--- paem-dropbox-js.js
+++ paem-dropbox-js.js
@@ -9,7 +9,7 @@ DEPENDENCIES:
  * @param pRedirectUri -- example 'https://yyy.github.io/xxx/'
  * @returns {Promise<any>}
  */
-function paemAuth(pClientId, pRedirectUri) {
+function paemDropboxAuth(pClientId, pRedirectUri) {
   return new Promise((resolve, reject) => {
     try {
       dropbox.authenticate({ client_id: pClientId, redirect_uri: pRedirectUri, },
@@ -20,4 +20,31 @@ function paemAuth(pClientId, pRedirectUri) {
       reject(e);
     }
   })
+}
+
+/**
+ * Returns content of file (1)
+ *
+ * @param filePath {String} - example '/file.txt'
+ * @returns {Promise<String>}
+ */
+function paemDropboxFileDownload(filePath) {
+  return new Promise((resolve, reject) => {
+    dropbox('files/download', { path: filePath }, {
+      onComplete: function (result, resp) {
+        console.log('!!-!!-!! -> onComplete() {200322061108}'); // del+
+        console.log('!!-!!-!! result {200322060448}\n', result); // del+
+        console.log('!!-!!-!! resp {200322063834}\n', resp); // del+
+        resp.text()
+          .then(function (text) {
+            console.log('!!-!!-!! text {200322064020}\n', text); // del
+            resolve(text);
+          })
+          .catch(function (err) {
+            console.log('!!-!!-!! err {200322064035}\n', err); // del
+            reject(err);
+          })
+      },
+    });
+  });
 }
