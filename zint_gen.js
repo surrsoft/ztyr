a3e1436b8217d56ab67f1cc77aaf611bc321e038
surr <e5535@mail.ru>
2020-03-22 08:45:42 +0300

    cc

--- zint_gen.js
+++ zint_gen.js
@@ -2,37 +2,48 @@
 Модуль для генерации [zint]-терминов
  */
 
+const zintAlphabet1 = "abcdefghijkmnoprstuvwxyz";
+const zintAlphabet2 = "abcdefghijkmnoprtuvwxyz"; //без s
+
 //генерирует английский [zint]-термин
 function zintGenerateEng() {
-    const stAlphabet1 = "abcdefghijkmnoprstuvwxyz";
-    const stAlphabet2 = "abcdefghijkmnoprtuvwxyz"; //без s
-    //---
-    const r_1 = zintRandomInt(0, stAlphabet1.length - 1);
-    const r_2 = zintRandomInt(0, stAlphabet1.length - 1);
-    const r_3 = zintRandomInt(0, stAlphabet1.length - 1);
-    const r_4 = zintRandomInt(0, stAlphabet2.length - 1);
-    return stAlphabet1.charAt(r_1) + stAlphabet1.charAt(r_2) + stAlphabet1.charAt(r_3) + stAlphabet2.charAt(r_4);
+  const r_1 = zintRandomInt(0, zintAlphabet1.length - 1);
+  const r_2 = zintRandomInt(0, zintAlphabet1.length - 1);
+  const r_3 = zintRandomInt(0, zintAlphabet1.length - 1);
+  const r_4 = zintRandomInt(0, zintAlphabet2.length - 1);
+  return zintAlphabet1.charAt(r_1) + zintAlphabet1.charAt(r_2) + zintAlphabet1.charAt(r_3) + zintAlphabet2.charAt(r_4);
 }
 
 //генерирует кирилический [zint]-термин
 function zintGenerateRu() {
-    const st1 = "абвгдезиклмнопрстуфхюя";
-    const st2 = "абвгдезиклмнопрстуфхюя";
-    //---
-    const r_1 = zintRandomInt(0, st1.length - 1);
-    const r_2 = zintRandomInt(0, st1.length - 1);
-    const r_3 = zintRandomInt(0, st1.length - 1);
-    const r_4 = zintRandomInt(0, st2.length - 1);
-    const genSt = st1.charAt(r_1) + st1.charAt(r_2) + st1.charAt(r_3) + st2.charAt(r_4);
+  const st1 = "абвгдезиклмнопрстуфхюя";
+  const st2 = "абвгдезиклмнопрстуфхюя";
+  //---
+  const r_1 = zintRandomInt(0, st1.length - 1);
+  const r_2 = zintRandomInt(0, st1.length - 1);
+  const r_3 = zintRandomInt(0, st1.length - 1);
+  const r_4 = zintRandomInt(0, st2.length - 1);
+  const genSt = st1.charAt(r_1) + st1.charAt(r_2) + st1.charAt(r_3) + st2.charAt(r_4);
 }
 
 //генерирует рандомное целое число в диапазоне [min] [max]
 function zintRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function zintVerifyName(name) {
   if (name && name.length === 4) {
+    name.forEach((el, index) => {
+      if (index < 3) {
+        if (zintAlphabet1.indexOf(el) === -1) {
+          return false;
+        }
+      } else {
+        if (zintAlphabet2.indexOf(el) === -1) {
+          return false;
+        }
+      }
+    });
     return true;
   }
   return false;
